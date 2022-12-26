738d0aa1638fc740674723bd751e20318103345c
Marvin Zhang <tikazyq@163.com>
2022-12-26 14:10:54 +0800

    feat: updated spiders

--- crawlab_demo/data/spiders/python/bookdepository/bookdepository/items.py
+++ crawlab_demo/data/spiders/python/bookdepository/bookdepository/items.py
@@ -9,6 +9,10 @@ import scrapy
 class BookItem(scrapy.Item):
     # define the fields for your item here like:
     title = scrapy.Field()
+    image = scrapy.Field()
+    url = scrapy.Field()
     author = scrapy.Field()
     price = scrapy.Field()
     published_date = scrapy.Field()
+    excerpt = scrapy.Field()
+    product_details = scrapy.Field()
--- crawlab_demo/data/spiders/python/bookdepository/bookdepository/spiders/books.py
+++ crawlab_demo/data/spiders/python/bookdepository/bookdepository/spiders/books.py
@@ -1,4 +1,5 @@
 import os
+from urllib.parse import urljoin
 
 import scrapy
 
@@ -14,17 +15,28 @@ class BooksSpider(scrapy.Spider):
     def parse(self, response: scrapy.http.Response, **kwargs):
         for item in response.css('.book-item'):
             title = item.css('.item-info h3.title a::text').extract_first()
+            url = urljoin(response.url, item.css('.item-info h3.title a::attr("href")').extract_first())
+            image = item.css('.item-img img::attr("data-lazy")').extract_first()
             author = item.css('.item-info p.author a > span::text').extract_first()
             price = item.css('.item-info .sale-price::text').extract_first()
             published_date = item.css('.item-info p.published::text').extract_first()
-            yield BookItem(
+            book_item = BookItem(
                 title=title,
+                url=url,
+                image=image,
                 author=author,
                 price=price,
                 published_date=published_date,
             )
+            yield scrapy.Request(url, callback=self.parse_item, meta={'book_item': book_item})
 
         next_url = response.css('li.next a::attr(href)').extract_first()
         yield scrapy.Request(
             url=f'https://www.bookdepository.com{next_url}',
         )
+
+    def parse_item(self, response: scrapy.http.Response, **kwargs):
+        book_item = response.meta['book_item']
+        book_item['excerpt'] = response.css('.item-excerpt::text').extract_first()
+        book_item['product_details'] = response.css('.biblio-wrap').extract_first()
+        yield book_item
